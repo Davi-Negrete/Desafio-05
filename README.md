ad2ea2f1119fe0dc196f914b97f0ed1b3e7ef997
Davi-Negrete <luisdavidnegrete12@gmail.com>
2024-04-10 16:24:30 -0500

    Agrego el Desafio-05 al repositorio

---
+++ README.md
@@ -0,0 +1,9 @@
+# Luis David Negrete Otero
+
+## Desafio-05 
+
+### Link al mi repositorio de GitHub: 
+
+
+###  Link a mi Netlify: https://venerable-dusk-360a7f.netlify.app/
+
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import {resolve} from "node:path"
+
+export default {
+    build:{
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                mariokart: resolve ("pages/mariokart.html"),
+                mortalkombat: resolve ("pages/mortalkombat.html"),
+                index: resolve ("index.html")        
+            }
+        }
+    }
+}
+
